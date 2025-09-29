0f1b444172996b522810892764682be9f1efec2a
Twilight <2475911580@qq.com>
2025-09-29 17:09:58 +0800

    fix: 打包路径

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/admin/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
